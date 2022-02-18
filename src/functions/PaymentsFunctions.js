6e2aa8f03aa2d2258c3e539e406c6528f34328b9
Alberto Molina Arribere <amolina@vaudoise.ch>
2022-02-18 16:23:34 +0100

    payments function added

---
+++ src/functions/PaymentsFunctions.js
@@ -0,0 +1,29 @@
+const Aux = require("./AuxiliaryFunctions.js");
+
+
+export var TokenAddress = "";
+export var PendingTokenAddress = "";
+
+export async function RetrieveTokenAddress(contract){
+    try{
+      RegistryAddress = await contract.methods.retrieveSettings().call();
+    }
+    catch(e) { 
+      window.alert("error retrieving the token address : " + JSON.stringify(e)); 
+    }
+    
+  }
+
+  export async function RetrievePendingTokenAddress(contract){
+    try{
+      let result = await contract.methods.retrieveProposition().call();
+      PendingTokenAddress = "-"
+      
+      if(result[0] != undefined)PendingTokenAddress = Aux.Bytes32ToAddress(result[0])
+    }
+    catch(e) { 
+      window.alert("error retrieving the pending token address : " + JSON.stringify(e)); 
+    }
+    
+  }
+    
