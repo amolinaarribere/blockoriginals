30b9b940c006d55a81d365962c92f24bfa83a334
Alberto Molina Arribere <amolina@vaudoise.ch>
2022-03-29 15:36:27 +0200

    commit

--- src/Demo.js
+++ src/Demo.js
@@ -28,7 +28,6 @@ const PiggyBank = "PiggyBank";
 
 class Demo extends React.Component {
   async componentWillMount() {
-    //this.state.loading = true;
     this.setState({loading: true})
     let currentTab = BrowserStorageFunctions.ReadKey(BrowserStorageFunctions.currentTabKey);
     if(currentTab){
@@ -43,7 +42,6 @@ class Demo extends React.Component {
     }
 
     this.refresh = this.refresh.bind(this)
-    //this.state.loading = false;
     this.setState({loading: false})
  }
 
