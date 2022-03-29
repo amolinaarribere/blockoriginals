dfd44c11ad4303bd3fcd580b08b05f873846b86a
Alberto Molina Arribere <amolina@vaudoise.ch>
2022-03-29 14:43:16 +0200

    second commit

--- src/functions/Constants.js
+++
@@ -1,5 +0,0 @@
-export const addressDataType = "address";
-export const intDataType = "int";
-export const numberDataType = "number";
-export const stringDataType = "string";
-export const bytesDataType = "bytes";
